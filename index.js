7c5e8de1319d9966c0368ba89a27cd9d80002c01
wesleygdike <wesley.g.dike@gmail.com>
2017-11-11 20:22:23 -0700

    Working locally with sockets

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+'use strict';
 
-// Activate the server to listen
-app.set('port', (process.env.PORT || 5000));
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-// Set static directory to public
-app.use(express.static(__dirname + '/public'));
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-// Landing Page
-app.get('/', function(request, response) {
-    console.log("Received Request");
-  response.render('pages/index');
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-// WEB SOCKETS
-
-// Register a callback function run when a new connection connects
-    io.on('connection', function(socket){
-        console.log('a user connected');
-        socket.on('disconnect', function(){
-          console.log('user disconnected');
-    });
-    });
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
